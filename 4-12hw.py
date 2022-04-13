87ca395fde60b950b81f9dcdfb10a0248c2b3413
kotaiyen <103197245+kotaiyen@users.noreply.github.com>
2022-04-13 23:02:04 +0800

    Update 4-12hw.py

--- 4-12hw.py
+++ 4-12hw.py
@@ -27,17 +27,9 @@ for i in range(9,0,-2):
         print(i,end='')
     print()
 
-print()
 
-#homework4
-for i in range(2,101):
-    s=0
-    for j in range(1,i+1):
-        if i%j==0:
-            s+=1
-    if s==2:
-        print (i)
-print()   
+
+
 
 
   
